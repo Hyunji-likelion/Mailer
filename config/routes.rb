303dd83a6d98a8a7a88e5561f5e91be644daefe1
hyunji <hyunji015@gmail.com>
2018-10-28 12:11:43 +0000

    routes 오타 수정

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   post 'mailbox/send_mail'
 
-  root 'home/index'
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
